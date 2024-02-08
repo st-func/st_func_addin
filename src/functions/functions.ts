78af5d7e94302161be1d056690bcc609fdd2514a
st_func <st_func@outlook.jp>
2024-02-08 22:43:46 +0900

    secBuildHの計算をパッケージ利用に変更

--- src/functions/functions.ts
+++ src/functions/functions.ts
@@ -1,4 +1,5 @@
-﻿/**
+﻿import { SecBuildHFunction } from "@st-func/st-func-ts";
+/**
  *  組立H形鋼の断面性能。
  * @customfunction secBuildH secBuildH
  * @param propertyType 表示したい断面性能のタイプ
@@ -9,15 +10,13 @@
  * @returns 断面性能
  */
 export function secBuildH(propertyType: string, a: number, b: number, t1: number, t2: number): number {
-  const a_in: number = a - 2 * t2;
-  const b_in: number = b - t1;
   switch (propertyType) {
     case "A":
-      return b * a - b_in * a_in;
+      return SecBuildHFunction.build_h_area(a, b, t1, t2);
     case "Iy":
-      return (b * a ** 3 - b_in * a_in ** 3) / 12.0;
+      return SecBuildHFunction.build_h_second_moment_of_area_y(a, b, t1, t2);
     case "Iz":
-      return (t2 * 2 * b ** 3 + a_in * t1 ** 3) / 12.0;
+      return SecBuildHFunction.build_h_second_moment_of_area_z(a, b, t1, t2);
     default: {
       let error = new CustomFunctions.Error(
         CustomFunctions.ErrorCode.invalidValue,
