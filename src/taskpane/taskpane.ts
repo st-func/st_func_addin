f6de7f9f1fde8abc37dfcbe4b11f1b061836d1c4
st_func <st_func@outlook.jp>
2024-02-18 20:39:12 +0900

    taskpaneの修正

--- src/taskpane/taskpane.ts
+++ src/taskpane/taskpane.ts
@@ -9,27 +9,4 @@
 Office.onReady(() => {
   document.getElementById("sideload-msg").style.display = "none";
   document.getElementById("app-body").style.display = "flex";
-  document.getElementById("run").onclick = run;
 });
-
-export async function run() {
-  try {
-    await Excel.run(async (context) => {
-      /**
-       * Insert your Excel code here
-       */
-      const range = context.workbook.getSelectedRange();
-
-      // Read the range address
-      range.load("address");
-
-      // Update the fill color
-      range.format.fill.color = "yellow";
-
-      await context.sync();
-      console.log(`The range address was ${range.address}.`);
-    });
-  } catch (error) {
-    console.error(error);
-  }
-}
